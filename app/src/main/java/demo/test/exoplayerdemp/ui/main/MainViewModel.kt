6ae6721000bae0a61557c583cc9b8c1d83151626
Aparna Thampy <aparna.thampy@robosoftin.com>
2023-04-28 15:12:10 +0530

    Firebase analytics implementation

--- app/src/main/java/demo/test/exoplayerdemp/ui/main/MainViewModel.kt
+++ app/src/main/java/demo/test/exoplayerdemp/ui/main/MainViewModel.kt
@@ -1,9 +1,14 @@
 package demo.test.exoplayerdemp.ui.main
 
+import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import demo.test.exoplayerdemp.base.BaseViewModel
 import demo.test.exoplayerdemp.ui.main.model.CountData
+import demo.test.exoplayerdemp.utility.Constant
 
 class MainViewModel() : BaseViewModel() {
     val countData: MutableLiveData<CountData> by lazy {
@@ -16,10 +21,14 @@ class MainViewModel() : BaseViewModel() {
             it.value = View.VISIBLE
         }
     }
+    private  var analytics: FirebaseAnalytics = Firebase.analytics
 
     fun incrementPausedCount() {
         countData.value = countData.value?.apply {
             pauseCount++
+            analytics.setDefaultEventParameters(Bundle().apply {
+                this.putInt(Constant.AnalyticsKEy.pauseCount, pauseCount)
+            })
         }
     }
 
--- app/src/main/java/demo/test/exoplayerdemp/utility/Constant.kt
+++ app/src/main/java/demo/test/exoplayerdemp/utility/Constant.kt
@@ -1,5 +1,9 @@
 package demo.test.exoplayerdemp.utility
 
 object Constant {
+    object AnalyticsKEy {
+        const val pauseCount= "pause_count"
+    }
+
     const val PLAYER_URL = "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"
 }
