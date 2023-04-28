1d0fac21f906b9af6f455bdeb1a5857a3d2c5289
Aparna Thampy <aparna.thampy@robosoftin.com>
2023-04-28 14:41:26 +0530

    exoplayer implementation

---
+++ app/src/main/java/demo/test/exoplayerdemp/ui/main/model/CountData.kt
@@ -0,0 +1,7 @@
+package demo.test.exoplayerdemp.ui.main.model
+
+data class CountData(
+    var pauseCount:Int = 0,
+    var forwardCount:Int = 0,
+    var backwardCount:Int = 0
+)
---
+++ app/src/main/java/demo/test/exoplayerdemp/utility/Constant.kt
@@ -0,0 +1,5 @@
+package demo.test.exoplayerdemp.utility
+
+object Constant {
+    const val PLAYER_URL = "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"
+}
